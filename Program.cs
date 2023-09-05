0eceb072b29b711a2ac0506204eaa45aa7c450dd
Tadeo Alonso Ruiz <alonsoruiztadeo26@gmail.com>
2023-09-05 20:05:06 -0300

    Actualización del Menu

--- Program.cs
+++ Program.cs
@@ -11,12 +11,13 @@ internal class Program{
         cadeteria.cargarCadetesCSV(archivo);
 
         int opcion = 0;
-        while(opcion != 5){
+        while(opcion != 6){
             Console.WriteLine("1- Agregar un pedido");
             Console.WriteLine("2- Asignar un cadete a un pedido");
             Console.WriteLine("3- Cambiar el estado de un pedido");
-            Console.WriteLine("4- Mostrar el informe");
-            Console.WriteLine("5- Salir");
+            Console.WriteLine("4- Mostrar el jornal de un cadete");
+            Console.WriteLine("5- Reasignar un pedido a otro cadete");
+            Console.WriteLine("6- Salir");
             bool successfullyParsed = int.TryParse(Console.ReadLine(), out opcion);
 
             switch(opcion){
@@ -43,6 +44,16 @@ internal class Program{
                     successfullyParsed = int.TryParse(Console.ReadLine(), out id);
                     int jornal = cadeteria.jornalACobrar(id);
                     Console.WriteLine($"El jornal a cobrar es {jornal}");
+                    break;
+                case 5:
+                    Console.WriteLine("Ingrese el ID del nuevo cadete");
+                    successfullyParsed = int.TryParse(Console.ReadLine(), out id);
+                    Console.WriteLine("Ingrese el número del nuevo pedido");
+                    successfullyParsed = int.TryParse(Console.ReadLine(), out numero);
+                    cadeteria.reasignarCadete(id, numero);
+                    break;
+                default:
+                    Console.WriteLine("Error");
                     break;
             }
         }
